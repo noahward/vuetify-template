c76d58a0705dd23bc24f7f0e70af0a03178b3858
Noah <noahward@shaw.ca>
2023-07-29 10:07:33 -0400

    Add catch-all route

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/:pathMatch(.*)*',
+      name: 'catch',
+      component: HomeView
+    },
+    {
       path: '/',
       name: 'home',
       component: HomeView
@@ -12,9 +17,6 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
     }
   ]
