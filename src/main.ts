83c8b32646cd8b4ba027498ea6d6347708753cb1
Noah <noahward@shaw.ca>
2023-07-29 10:25:13 -0400

    Organize scss

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
+import router from '@/router'
 import vuetify from '@/plugins/vuetify'
 import PerfectScrollbar from '@/plugins/perfect-scrollbar'
+
+import '@/scss/style.scss'
 
 const app = createApp(App)
 
